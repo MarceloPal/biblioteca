e87c1c7e2d6a8a1495715c35f835b09856e6e357
Marcelo <mar.palmal@duocuc.cl>
2025-04-02 21:08:05 -0300

    sexto commit con cambios que envio el profesor

--- biblioteca/src/main/java/com/example/biblioteca/repository/LibroRepository.java
+++ biblioteca/src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -10,7 +10,13 @@ public class LibroRepository {
     
     @PostConstructor
     public void init (){
-        listaLibros.add(new Libro(2,"978-1617","Spring in action","Addison Wesley"))
+        listaLibros.add(new Libro(1, "978-0134685991", "Effective Java", "Addison-Wesley", 2018, "Joshua Bloch"));
+
+        listaLibros.add(new Libro(2, "978-1617294945", "Spring in Action", "Manning", 2020, "Craig Walls"));
+
+        listaLibros.add(new Libro(3, "978-1491950357", "Designing Data-Intensive Applications", "O'Reilly Media", 2017, "Martin Kleppmann"));
+
+        listaLibros.add(new Libro(4, "978-0132350884", "Clean Code", "Prentice Hall", 2008, "Robert C. Martin"));
 
     }
 
